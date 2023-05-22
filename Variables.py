71603f6f4ce3eb1d79117079cbe98af73c1d6d54
Ganesh <ganeshnikam707@gmail.com>
2023-05-22 10:37:16 +0530

    frist commit

---
+++ Variables.py
@@ -0,0 +1,16 @@
+# assign multiple values to multiple variables.......
+
+a,b,c = 5,3.2,"Hello"           # output....
+print(a)                        # 5
+print(b)                        # 3.2
+print(c)                        # Hello
+
+
+# assign singel value to multiple variables....
+a=b=c = 2.54            #output....
+print(a)                  #2.54
+print(b)                  #2.54
+print(c)                  #2.54
+
+
+
