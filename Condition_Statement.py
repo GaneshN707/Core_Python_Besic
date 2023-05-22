49dc70cccb80bc4cf5486791491ba0f23d759226
Ganesh <ganeshnikam707@gmail.com>
2023-05-22 12:20:26 +0530

    Add New File

---
+++ Condition_Statement.py
@@ -0,0 +1,35 @@
+#condition Statement....
+
+#if Stetement....
+
+a=10
+b=5
+if a>b:
+    print("a is Greater than b")
+
+
+# if Else Statement....
+a=3
+b=5
+if a>b:
+    print("a is Greater than b")
+else:
+    print("b is greater than a")
+
+#if elif else statement....
+a= 7
+
+if a> 10:
+    print("a is greater than 10")
+elif a > 5:
+    print("a is greater than 5 ")
+else:
+    print("a is less than 5")
+
+#Nested if Statement.....
+a = 5
+
+if a > 0:
+    if a < 10:
+        print("a is a positive number")
+
