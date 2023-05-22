aea6e7bd456dd2d2fcfeb5d86e823e5142b1b219
Ganesh <ganeshnikam707@gmail.com>
2023-05-22 10:58:23 +0530

    frist commit

---
+++ Arithmetic_Operator.py
@@ -0,0 +1,26 @@
+#Arithmetic Operator......
+
+# addition..
+a=3
+b=7
+print(a+b)     #10
+
+# Subtraction...
+a=7
+b=3
+print(a-b)    #4
+
+# Multiplication
+a=7
+b=3
+print(a*b)     #21
+
+#division...
+a=7
+b=3
+print(a/b)   #2.33
+
+#modulus....
+a=10
+b=3
+print(a%b)    #1
