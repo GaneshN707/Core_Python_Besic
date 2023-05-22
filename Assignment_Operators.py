6e6ce2a3bd1bbed2addf2439325b9b65ee45d801
Ganesh <ganeshnikam707@gmail.com>
2023-05-22 11:37:47 +0530

    add AssignmetOperators commit

---
+++ Assignment_Operators.py
@@ -0,0 +1,35 @@
+#assignment Operators....
+
+#Assignment operator..
+a=5
+print(a)   #output....  5
+
+#add And Assing...
+a=5
+a+=1
+print(a)  #output....  6
+
+#Substract and assign..
+a=5
+a-=1
+print(a)  #output....  4
+
+#divide and assign
+a=5
+a/=2
+print(a)  #output....  2.5
+
+#multiply and assign...
+a=5
+a*=2
+print(a)  #output....  10
+
+#modulus and assing..
+a=5
+a%=2
+print(a)  #output....  1
+
+
+
+
+
