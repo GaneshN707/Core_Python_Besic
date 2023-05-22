c58d962b7c1c1aa69b2c0b59eae3d800fc204eca
Ganesh <ganeshnikam707@gmail.com>
2023-05-22 11:13:36 +0530

    add Newfile

---
+++ Relational_Operators.py
@@ -0,0 +1,30 @@
+# Relational Operators.....
+#Equality....
+a = 5
+b = 3
+print(a==b)  #output.. False
+
+#inequality...
+a = 5
+b = 3
+print(a!=b)   # Output.. True
+
+#Greater than....
+a = 5
+b = 3
+print(a>b)   # Output.. True
+
+#less than....
+a = 5
+b = 3
+print(a<b)   # Output.. False
+
+#Greater tnan or Equal.....
+a = 5
+b = 3
+print(a>=b)   # Output.. True
+
+#Less than or Equal to...
+a = 5
+b = 3
+print(a<=b)   # Output.. false
