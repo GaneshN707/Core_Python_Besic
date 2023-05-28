f1bbad8cbd260e126ee76bae1ee452aa098deddb
Ganesh <ganeshnikam707@gmail.com>
2023-05-28 15:36:30 +0530

    Commit

---
+++ TypeCasting.py
@@ -0,0 +1,16 @@
+
+# EXPLICIT CONVERSION,..,.,.,.,
+
+string="15"
+number = 7
+string_number= int(string)
+sum=number+string_number
+print(sum)
+
+# IMPLICIT CONVERSION,.,.,.,.,.
+a = 7
+print(type(a))
+b=3.0
+print(type(b))
+c=a+b
+print(c)
